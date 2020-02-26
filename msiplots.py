4743551651df9764153999542a681dc3db1e6de1
Clodagh McGuire <clodagh.mcguire@nhs.net>
2020-02-26 15:40:53 +0000

    select directory and save graphs

--- msiplots.py
+++ msiplots.py
@@ -10,19 +10,16 @@ from tkinter.filedialog import askopenfilename
 
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 print(filename)
-
+print("where do you want to save your graphs?")
+directory = tkinter.filedialog.askdirectory()
 with open(filename) as f:
     read_data = f.readlines() # reads each line in file
-    #print(read_data)
     for line in read_data:
-        #print(line)
         c = line.startswith("chr")
         if c is True:
             graph_title = line.strip()
         s = line.startswith("T:") # looks for lines that start with T: as these are the lines with read count distribution
-        #print(s)
         if s is True:
-            #print(line)
             #remove T: from start of line and turn line into list of strings
             data = line.strip("T:")
             data = data.split()
@@ -33,5 +30,9 @@ with open(filename) as f:
                 plt.plot(data) # plots list of numbers (ie read count distribution) as a line graph
                 plt.xlabel("Repeat length")
                 plt.ylabel("Frequency")
-                plt.show()
+                plt.title(graph_title)
+                plt.savefig(directory+"/"+graph_title+".png")
+                plt.clf() #clears plot from axis so that it's blank for the next plot
+
                 
+
